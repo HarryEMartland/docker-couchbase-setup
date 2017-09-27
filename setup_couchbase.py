f13d2f2dd30f465211a2d551be272c9b3144a9da
martlandh <HarryMartland@Rentalcars.com>
2017-09-27 21:57:04 +0100

    Added ability to create views

--- setup_couchbase.py
+++ setup_couchbase.py
@@ -1,10 +1,12 @@
 import os
-import requests
 import time
+
+import requests
 
 BUCKET_PREFIX = "COUCHBASE_BUCKET_"
 BUCKET_NAME_SUFFIX = "_NAME"
 BUCKET_PASSWORD_SUFFIX = "_PASSWORD"
+BUCKET_VIEWS_SUFFIX = "_VIEWS"
 
 couchbase_host = os.environ['COUCHBASE_HOST']
 couchbase_port = os.environ['COUCHBASE_PORT']
@@ -37,12 +39,24 @@ for env in os.environ:
         bucketPassword = os.environ[BUCKET_PREFIX + bucketKey + BUCKET_PASSWORD_SUFFIX]
         print("Creating bucket " + bucketKey + " As " + bucketName)
 
-        requests.post("http://" + couchbase_host + ":" + couchbase_port + "/pools/default/buckets", data={
-            'name': bucketName,
-            'ramQuotaMB': "200",
-            'saslPassword': bucketPassword,
-            'proxyPort': "11214",
-            'authType': 'sasl'
-        }, auth=auth).raise_for_status()
+        requests.post("http://" + couchbase_host + ":" + couchbase_port + "/pools/default/buckets",
+                      data={
+                          'name': bucketName,
+                          'ramQuotaMB': "200",
+                          'saslPassword': bucketPassword,
+                          'proxyPort': "11214",
+                          'authType': 'sasl'
+                      }, auth=auth).raise_for_status()
+
+        viewsDir = os.environ.get(BUCKET_PREFIX + bucketKey + BUCKET_VIEWS_SUFFIX)
+        if (viewsDir):
+            time.sleep(5)
+            for (filenames) in os.listdir(viewsDir):
+                print("Creating view " + filenames)
+                with open(viewsDir + "/" + filenames, 'r') as myfile:
+                    viewJs = myfile.read()
+                    requests.put(
+                        "http://" + couchbase_host + ":8092" + "/" + bucketName + "/_design/_" + filenames[:-5],
+                        json=viewJs, auth=auth)
 
 print("couchbase setup")
